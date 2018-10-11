e460e81d939f131330923bfe4a919cacb08fd5f0
Michael Smith <strikemike2k@gmail.com>
2018-10-11 10:32:40 -0400

    Function to draw background

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,18 @@
 import SpriteSheet from './sprite_sheet.js'
 import {loadImage, loadLevel} from './loaders.js';
 
+function drawBackground(background, context, sprites) {
+  background.ranges.forEach(([x1, x2, y1, y2]) => {
+    for (let x = x1; x < x2; x++) {
+      for (let y = y1; y < y2; y++) {
+        sprites.drawTile(background.tile, context, x, y);
+      }
+    }
+  });
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
-
-context.fillRect(0,0,50,50);
 
 loadImage('images/tileset.png')
 .then(image => {
@@ -15,13 +23,9 @@ loadImage('images/tileset.png')
   loadLevel('1-1')
   .then(level => {
     console.log(level);
+    drawBackground(level.backgrounds[0], context, sprites);
   });
   
-  for (let x = 0; x < 25; x++) {
-    for (let y = 0; y < 14; y++) {
-      sprites.drawTile('sky', context, x, y);
-    }
-  }
   for (let x = 0; x < 25; x++) {
     for (let y = 12; y < 14; y++) {
       sprites.drawTile('ground', context, x, y);
