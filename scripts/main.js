35a9cf5fe7e6ea44fc6c6a2b30bc72b9acce1dd1
Michael Smith <strikemike2k@gmail.com>
2018-10-11 10:09:27 -0400

    Draw tile with SpriteSheet class

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,29 @@ function loadImage(url) {
   });
 }
 
+class SpriteSheet {
+  constructor(image, width, height) {
+    this.image = image;
+    this.width = width;
+    this.height = height;
+    this.tiles = new Map();
+  }
+  
+  define(name, x, y) {
+    const buffer = document.createElement('canvas');
+    buffer.width = this.width;
+    buffer.height = this.height;
+    buffer.getContext('2d')
+      .drawImage(this.image, x * this.width, y * this.height, this.width, this.height, 0, 0, this.width, this.height);
+    this.tiles.set(name, buffer);
+  }
+  
+  draw(name, context, x, y) {
+    const buffer = this.tiles.get(name);
+    context.drawImage(buffer, x, y);
+  }
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
@@ -15,5 +38,9 @@ context.fillRect(0,0,50,50);
 
 loadImage('images/tileset.png')
 .then(image => {
+  const sprites = new SpriteSheet(image, 16, 16);
+  sprites.define('ground', 0, 0);
+  sprites.draw('ground', context, 45, 62);
+  
   context.drawImage(image, 0, 0, 16, 16, 0, 0, 16, 16);
 });
