88613e8273fa5b52fd2e0a641a77b283c3187b4c
Michael Smith <strikemike2k@gmail.com>
2018-11-09 15:10:45 -0500

    draw background from buffer

--- scripts/main.js
+++ scripts/main.js
@@ -20,8 +20,12 @@ Promise.all([
   loadLevel('1-1')
 ])
 .then(([marioSprite, sprites, level]) => {
+  const backgroundBuffer = document.createElement('canvas');
+  backgroundBuffer.width = 256;
+  backgroundBuffer.height = 240;
+
   level.backgrounds.forEach(background => {
-    drawBackground(background, context, sprites);
+    drawBackground(background, backgroundBuffer.getContext('2d'), sprites);
   });
 
   const pos = {
@@ -30,6 +34,7 @@ Promise.all([
   };
 
   function update() {
+    context.drawImage(backgroundBuffer, 0, 0);
     marioSprite.draw('idle', context, pos.x, pos.y);
     pos.x += 2;
     pos.y += 2;
