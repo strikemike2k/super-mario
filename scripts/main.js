8f2d4358664f6f884279567f2ee51822fdbbe886
Michael Smith <strikemike2k@gmail.com>
2018-11-09 14:41:34 -0500

    Refactor to load async promises

--- scripts/main.js
+++ scripts/main.js
@@ -11,21 +11,27 @@ function drawBackground(background, context, sprites) {
   });
 }
 
+function loadBackgroundSprites() {
+  return loadImage('images/tileset.png')
+  .then(image => {
+    console.log('Image loaded', image);
+    const sprites = new SpriteSheet(image, 16, 16);
+    sprites.define('ground', 0, 0);
+    sprites.define('sky', 3, 23);
+    return sprites;
+  });
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-loadImage('images/tileset.png')
-.then(image => {
-  console.log('Image loaded', image);
-  const sprites = new SpriteSheet(image, 16, 16);
-  sprites.define('ground', 0, 0);
-  sprites.define('sky', 3, 23);
-
+Promise.all([
+  loadBackgroundSprites(),
   loadLevel('1-1')
-  .then(level => {
-    console.log('Level loaded', level);
-    level.backgrounds.forEach(background => {
-      drawBackground(background, context, sprites);
-    });
+])
+.then(([sprites, level]) => {
+  console.log('Level loaded', level);
+  level.backgrounds.forEach(background => {
+    drawBackground(background, context, sprites);
   });
 });
