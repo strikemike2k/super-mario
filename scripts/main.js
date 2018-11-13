00bac1c30edb0bbcf367a8178e001197558e83b0
Michael Smith <strikemike2k@gmail.com>
2018-11-13 13:28:31 -0500

    Create Entity class but we need a set function

--- scripts/main.js
+++ scripts/main.js
@@ -19,6 +19,13 @@ class Vec2 {
   }
 }
 
+class Entity {
+  constructor() {
+    this.pos = new Vec2(0, 0);
+    this.vel = new Vec2(0, 0);
+  }
+}
+
 Promise.all([
   loadMarioSprite(),
   loadBackgroundSprites(),
@@ -31,6 +38,10 @@ Promise.all([
   comp.layers.push(backgroundLayer);
 
   const gravity = 0.5;
+
+  const mario = new Entity();
+  mario.pos.x = 64;
+  mario.pos.y = 180;
 
   const pos = new Vec2(64, 180);
   const vel = new Vec2(2, -10);
