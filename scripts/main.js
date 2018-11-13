b9e4654eff1c417afc9311b2078b3175bd21af9d
Michael Smith <strikemike2k@gmail.com>
2018-11-13 14:19:02 -0500

    Move Vector to math.js and Entity to class file

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
-import Compositor from './compositor.js';
+import Compositor from './Compositor.js';
+import Entity from './Entity.js';
 import {loadLevel} from './loaders.js';
 import {loadMarioSprite, loadBackgroundSprites} from './sprites.js';
 import {createBackgroundLayer} from './layers.js';
@@ -12,23 +13,6 @@ function createSpriteLayer(entity) {
   }
 }
 
-class Vec2 {
-  constructor(x, y) {
-    this.set(x, y);
-  }
-
-  set(x, y){
-    this.x = x;
-    this.y = y;
-  }
-}
-
-class Entity {
-  constructor() {
-    this.pos = new Vec2(0, 0);
-    this.vel = new Vec2(0, 0);
-  }
-}
 
 Promise.all([
   loadMarioSprite(),
