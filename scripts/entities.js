86387d38f80a01afeec29b77aabd7f4d0c22f176
Michael Smith <strikemike2k@gmail.com>
2018-11-19 15:12:29 -0500

    Remove unneeded comments

--- scripts/entities.js
+++ scripts/entities.js
@@ -4,18 +4,16 @@ import {loadMarioSprite} from './sprites.js';
 export function createMario() {
   return loadMarioSprite()
   .then(sprite => {
-    // BEGIN exact copy from main.js
     const mario = new Entity();
 
     mario.draw = function drawMario(context) {
-      sprite.draw('idle', context, this.pos.x, this.pos.y); // change marioSprite to sprites
+      sprite.draw('idle', context, this.pos.x, this.pos.y);
     }
 
     mario.update = function updateMario(deltaTime) {
       this.pos.x += this.vel.x * deltaTime;
       this.pos.y += this.vel.y * deltaTime;
     }
-    // END exact copy from main.js
 
     return mario;
   });
