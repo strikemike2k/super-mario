5bf3da377c90d5d2c76eeab1c5d895f9719ec387
Michael Smith <strikemike2k@gmail.com>
2018-11-19 15:20:06 -0500

    Add trait to mario for velocity

--- scripts/entities.js
+++ scripts/entities.js
@@ -1,18 +1,27 @@
-import Entity from './Entity.js';
+import Entity, {Trait} from './Entity.js';
 import {loadMarioSprite} from './sprites.js';
+
+class Velocity extends Trait {
+
+  constructor() {
+    super('velocity');
+  }
+
+  update(entity, deltaTime){
+    entity.pos.x += entity.vel.x * deltaTime;
+    entity.pos.y += entity.vel.y * deltaTime;
+  }
+}
 
 export function createMario() {
   return loadMarioSprite()
   .then(sprite => {
     const mario = new Entity();
 
+    mario.addTrait(new Velocity());
+
     mario.draw = function drawMario(context) {
       sprite.draw('idle', context, this.pos.x, this.pos.y);
-    }
-
-    mario.update = function updateMario(deltaTime) {
-      this.pos.x += this.vel.x * deltaTime;
-      this.pos.y += this.vel.y * deltaTime;
     }
 
     return mario;
