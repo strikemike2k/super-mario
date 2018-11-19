35102ec14d0588f7ff029ca0221551cf88ebf07c
Michael Smith <strikemike2k@gmail.com>
2018-11-19 15:23:56 -0500

    Decouple Velocity trait into own file

--- scripts/entities.js
+++ scripts/entities.js
@@ -1,17 +1,6 @@
-import Entity, {Trait} from './Entity.js';
+import Entity from './Entity.js';
+import Velocity from './traits/Velocity.js';
 import {loadMarioSprite} from './sprites.js';
-
-class Velocity extends Trait {
-
-  constructor() {
-    super('velocity');
-  }
-
-  update(entity, deltaTime){
-    entity.pos.x += entity.vel.x * deltaTime;
-    entity.pos.y += entity.vel.y * deltaTime;
-  }
-}
 
 export function createMario() {
   return loadMarioSprite()
