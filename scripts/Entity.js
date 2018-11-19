0d2ac48ff0d6b981380761e125ec23a17d755600
Michael Smith <strikemike2k@gmail.com>
2018-11-19 15:11:53 -0500

    Create Trait class

--- scripts/Entity.js
+++ scripts/Entity.js
@@ -1,11 +1,26 @@
 import {Vec2} from './math.js';
 
+export class Trait {
+  constructor(name) {
+    this.NAME = name;
+  }
+
+  update() {
+    console.warn('Unhandled update call in Trait');
+  }
+}
+
 export default class Entity {
   constructor() {
     this.pos = new Vec2(0, 0);
     this.vel = new Vec2(0, 0);
 
     this.traits = [];
+  }
+
+  addTrait(trait) {
+    this.traits.push(trait);
+    this[trait.NAME] = trait;
   }
 
   update(deltaTime) {
