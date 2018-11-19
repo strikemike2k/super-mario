d1ef90ce517162180d3c0eaa0436a6e0410dfdba
Michael Smith <strikemike2k@gmail.com>
2018-11-19 14:32:00 -0500

    Simple keydown event listener

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,10 @@ import {createMario} from './entities.js';
 import {loadBackgroundSprites} from './sprites.js';
 import {createBackgroundLayer, createSpriteLayer} from './layers.js';
 
+window.addEventListener('keydown', event => {
+  console.log(event);
+});
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
