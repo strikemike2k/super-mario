e08b01093eaaffae541d20c0b087f109db54242b
Michael Smith <strikemike2k@gmail.com>
2018-12-21 15:09:20 -0500

    TileResolver

--- scripts/TileCollider.js
+++ scripts/TileCollider.js
@@ -1,8 +1,38 @@
+class TileResolver {
+  constructor(matrix, tileSize = 16) {
+    this.matrix = matrix;
+    this.tileSize = tileSize;
+  }
+
+  toIndex(pos) {
+    return Math.floor(pos / this.tileSize);
+  }
+
+  getByIndex(indexX, indexY) {
+    const tile = this.matrix.get(indexX, indexY);
+    if (tile) {
+      return { tile };
+    }
+  }
+
+  matchByPosition(posX, posY) {
+    return this.getByIndex(
+      this.toIndex(posX),
+      this.toIndex(posY));
+  }
+}
+
+window.TileResolver = TileResolver;
+
 export default class TileCollider {
-  constructor(tiles) {
-    this.tiles = tiles
+  constructor(tileMatrix) {
+    this.tiles = new TileResolver(tileMatrix);
   }
 
   test(entity) {
+    const match = this.tiles.matchByPosition(entity.pos.x, entity.pos.y);
+    if (match) {
+      console.log('Matched title', match, match.tile);
+    }
   }
 }
