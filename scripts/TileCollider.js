abd457120d37f28da288cb7b5b4b3a4cb48564d4
Michael Smith <strikemike2k@gmail.com>
2018-12-26 14:44:52 -0500

    Only check tiles that matter

--- scripts/TileCollider.js
+++ scripts/TileCollider.js
@@ -6,7 +6,13 @@ export default class TileCollider {
   }
 
   checkX(entity) {
-    const matches = this.tiles.searchByRange(entity.pos.x, entity.pos.x + entity.size.x, entity.pos.y, entity.pos.y + entity.size.y);
+    let x;
+    if (entity.vel.x > 0) {
+      x = entity.pos.x + entity.size.x;
+    } else if (entity.vel.x < 0) {
+      x = entity.pos.x;
+    } else return;
+    const matches = this.tiles.searchByRange(x, x, entity.pos.y, entity.pos.y + entity.size.y);
 
     matches.forEach(match => {
       if (match.tile.name !== 'ground') return;
@@ -25,7 +31,13 @@ export default class TileCollider {
   }
 
   checkY(entity) {
-    const matches = this.tiles.searchByRange(entity.pos.x, entity.pos.x + entity.size.x, entity.pos.y, entity.pos.y + entity.size.y);
+    let y;
+    if (entity.vel.y > 0) {
+      y = entity.pos.y + entity.size.y;
+    } else if (entity.vel.y < 0) {
+      y = entity.pos.y;
+    } else return;
+    const matches = this.tiles.searchByRange(entity.pos.x, entity.pos.x + entity.size.x, y, y);
 
     matches.forEach(match => {
       if (match.tile.name !== 'ground') return;
--- scripts/entities.js
+++ scripts/entities.js
@@ -8,7 +8,8 @@ export function createMario() {
   return loadMarioSprite()
   .then(sprite => {
     const mario = new Entity();
-    mario.size.set(14, 16);
+    //mario.size.set(14, 16);
+    mario.size.set(96, 96);
 
     mario.addTrait(new Go());
     mario.addTrait(new Jump());
